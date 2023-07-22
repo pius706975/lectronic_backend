2044e40945d46441a752f20f3e620e755abe858a
pius <piusrestiantoro2@gmail.com>
2023-07-22 15:18:38 +0800

    db connection created

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const appServer = express()
+const cors = require('cors')
+const response = require('./src/libs/responses')
+const router = require('./src/router/routers')
+const db = require('./src/database/db.config')
+const port = process.env.APP_PORT
+
+appServer.use(express.json())
+appServer.use(express.urlencoded({extended: true}))
+appServer.use(router)
+appServer.use(cors())
+
+appServer.all('*', (req, res, next)=>{
+    response(res, 404, 'Sorry!\n Page not found')
+})
+
+db.connect()
+.then(()=>{
+    console.log("DB connected")
+    appServer.listen(port, ()=>{
+        console.log(`Server is running on port ${port}`)
+    })
+}).catch((err)=>{
+    console.log(err)
+})
