beb247f6a0b722963afdb3905a86abf424fe5d20
pius <piusrestiantoro2@gmail.com>
2023-07-31 11:46:39 +0800

    [feat] Add middleware authentication Implemented authentication role to allow users to use all of the provided features based on their role whether they are an admin or user.
    -Added logic to check the user role while login, admin or user.

---
+++ src/middleware/middle.auth.js
@@ -0,0 +1,60 @@
+const response = require('../libs/responses')
+const jwt = require('jsonwebtoken')
+const middleware = {}
+
+middleware.authentication = async (req, res, next)=>{
+
+    try {
+        const authHeader = req.header.authorization
+        if (!authHeader) {
+            return response(res, 400, {message: 'You need to login first'})
+        }
+
+        const token = authHeader.split(' ')[1]
+        if (!token) {
+            return response(res, 40, {message: 'Token is required'})
+        }
+
+        const decoded = await jwt.verify(token, process.env.JWT_SECRET)
+
+        req.userData = decoded
+        console.log(decoded)
+
+        next()
+    } catch (error) {
+        console.log(error)
+        return response(res, 500, error)
+    }
+}
+
+middleware.isAdmin = async (req, res, next)=>{
+
+    try {
+        const user = req.userData
+        if (user.role === 'admin') {
+            next()
+        } else {
+            return response(res, 400, "You do not have permission")
+        }
+    } catch (error) {
+        console.log(error)
+        return response(res, 500, error)
+    }
+}
+
+middleware.isUser = async (req, res, next)=>{
+
+    try {
+        const user = req.userData
+        if (user.role === 'user') {
+            next()
+        } else {
+            return response(res, 400, "You do not have permission")
+        }
+    } catch (error) {
+        console.log(error)
+        return response(res, 500, error)
+    }
+}
+
+module.exports = middleware
